93f0fc7384d6839eaec0ab944cf1ff1e1bc253c0
AlexShem <alex.shemendyuk@gmail.com>
2025-09-01 18:23:23 +0200

    Stage 2 #OL4
    - Scanning: Parentheses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,27 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 
+enum TokenType {
+    LeftParen,
+    RightParen,
+    Unknown(String),
+}
+
+fn tokenize(content: String) -> Vec<TokenType> {
+    let chars = content.chars();
+    let mut tokens: Vec<TokenType> = Vec::with_capacity(content.len());
+
+    for char in chars {
+        let token = match char.to_string().as_str() {
+            "(" => TokenType::LeftParen,
+            ")" => TokenType::RightParen,
+            ch => TokenType::Unknown(ch.to_string()),
+        };
+        tokens.push(token);
+    }
+    tokens
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -20,7 +41,15 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                let tokens = tokenize(file_contents);
+                for token in tokens {
+                    match token {
+                        TokenType::LeftParen => println!("LEFT_PAREN ( null"),
+                        TokenType::RightParen => println!("RIGHT_PAREN ) null"),
+                        TokenType::Unknown(ch) => println!("Unknown Token {} null", ch),
+                    }
+                }
+                println!("EOF  null");
             } else {
                 println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
             }
